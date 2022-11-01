df0b1302e0dfb0a44432c35a2cbe6df97a1ca2b2
chenchao19934 <935626020@qq.com>
2022-11-01 10:45:13 +0800

    feat: init

---
+++ bin/heaven.js
@@ -0,0 +1,71 @@
+#!/usr/bin/env node
+const leven = require('leven')
+// 修改控制台字符串的样式
+const chalk = require('chalk')
+
+const program = require('commander')
+const init = require("../lib/init");
+
+program
+  .version(`${require('../package').version}`)
+  .usage('<command> [options]')
+
+
+program
+  .command('init <template> <app-name>')
+  .description('generate a project from a remote template (legacy API, requires @cc-cli)')
+  .option('-c, --clone', 'Use git clone when fetching remote template')
+  .option('--offline', 'Use cached template')
+  .action((template, name) => {
+    init(template, name)
+  })
+
+propgram
+  .command('list')
+  .description('template list')
+  .action(() => {
+    let table = new Table();
+
+  })
+
+ 
+// output help information on unknown commands
+program.on('command:*', ([cmd]) => {
+  program.outputHelp()
+  console.log(`  ` + chalk.red(`Unknown command ${chalk.yellow(cmd)}.`))
+  console.log()
+  suggestCommands(cmd)
+  process.exitCode = 1
+})
+
+// add some useful info on help
+program.on('--help', () => {
+  console.log()
+  console.log(`  Run ${chalk.cyan(`heaven <command> --help`)} for detailed usage of given command.`)
+  console.log()
+})
+
+program.commands.forEach(c => c.on('--help', () => console.log()))
+
+program.parse(process.argv)
+
+if (!process.argv.slice(2).length) {
+  program.outputHelp()
+}
+
+function suggestCommands (unknownCommand) {
+  const availableCommands = program.commands.map(cmd => cmd._name)
+
+  let suggestion
+
+  availableCommands.forEach(cmd => {
+    const isBestMatch = leven(cmd, unknownCommand) < leven(suggestion || '', unknownCommand)
+    if (leven(cmd, unknownCommand) < 3 && isBestMatch) {
+      suggestion = cmd
+    }
+  })
+
+  if (suggestion) {
+    console.log(`  ` + chalk.red(`Did you mean ${chalk.yellow(suggestion)}?`))
+  }
+}
